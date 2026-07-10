8a18214cf44c804138336b9535052c0c928095cc
kiddulu916 <dat1kidd916@gmail.com>
2026-07-09 21:37:12 -0700

    chore(observability): add Firebase Crashlytics/Analytics dependencies

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,6 +22,7 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
